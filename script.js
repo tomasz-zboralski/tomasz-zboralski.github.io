b448a0b5aaace4c5b013d6b052da981f72c5c023
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2021-01-28 08:56:52 +0100

    apiRoot fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'http://stark-tundra-57636.herokuapp.com/v1/task/';
+  var apiRoot = 'https://stark-tundra-57636.herokuapp.com/v1/task';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
