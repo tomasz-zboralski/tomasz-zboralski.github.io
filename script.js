3248151ab180769326760fe2eccfa565439c8940
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2021-02-13 11:13:26 +0100

    Trello integration urls fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'http://localhost:8080/v1/task/';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const apiRoot = 'https://stark-tundra-57636.herokuapp.com/v1/task/';
+   const trelloApiRoot = 'https://stark-tundra-57636.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
